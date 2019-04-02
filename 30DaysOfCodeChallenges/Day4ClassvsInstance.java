fbf9ce1c58de2716128084b8bdb11f03a022e714
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2019-04-02 22:05:10 +0200

    Update Day4ClassvsInstance.java

--- 30DaysOfCodeChallenges/Day4ClassvsInstance.java
+++ 30DaysOfCodeChallenges/Day4ClassvsInstance.java
@@ -1,1 +1,33 @@
 
+public class Person {
+    int age;    
+    public Person(int initial_Age) {
+        if(initial_Age<0){
+            age=0;
+            System.out.println("Age is not valid, setting age to 0.");
+                
+        }
+        else{
+            age=initial_Age;
+        }
+    }
+        
+
+    public void amIOld() {
+        String msg="";
+        
+        if(age<13)
+            msg="young";
+        else if (age == 16)
+            msg="a teenager";
+        else if (age >=13 && age < 18)
+            msg="a teenager";
+        else{
+            msg="old";
+        }
+        System.out.println("You are " + msg + ".");
+    }
+
+    public void yearPasses() {
+        age+=1;
+    }
