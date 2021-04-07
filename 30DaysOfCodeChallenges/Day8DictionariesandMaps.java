989a83c627225832287d4f583ec287df50562df6
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-04-07 08:38:22 +0200

    Update Day8DictionariesandMaps.java

--- 30DaysOfCodeChallenges/Day8DictionariesandMaps.java
+++ 30DaysOfCodeChallenges/Day8DictionariesandMaps.java
@@ -6,6 +6,7 @@ class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        Hashtable<String, int> my_dict = new Hashtable<String, int>();
         
         for(int i = 0; i < n; i++){
             String name = in.next();
