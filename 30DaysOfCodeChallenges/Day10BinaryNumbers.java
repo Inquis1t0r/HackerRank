3d5575ca5e6f32e564db278222c117ec0f0018b1
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-04-09 12:56:32 +0200

    Update Day10BinaryNumbers.java

--- 30DaysOfCodeChallenges/Day10BinaryNumbers.java
+++ 30DaysOfCodeChallenges/Day10BinaryNumbers.java
@@ -16,6 +16,19 @@ public class Solution {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        int binary[] = new int[40];    
+        int index = 0;    
+        
+        while(n > 0){    
+            binary[index++] = n%2;    
+            n = n/2;    
+        }    
+        
+        for(int i = index-1;i >= 0;i--){    
+            System.out.print(binary[i]);    
+        }    
+        System.out.println();
+        //TODO: Count consecutive 1's
         scanner.close();
     }
 }
