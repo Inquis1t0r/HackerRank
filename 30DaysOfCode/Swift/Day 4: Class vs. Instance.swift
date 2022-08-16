7cff58c95e2692a89c15eb5ad6f99a23829e4c66
Inquis1t0r <pi0tr3k9292@gmail.com>
2022-08-16 16:00:39 +0200

    Create Day 4: Class vs. Instance.swift

---
+++ 30DaysOfCode/Swift/Day 4: Class vs. Instance.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class Person {
+    var age: Int = 0
+
+    init(initialAge: Int) {
+        // Add some more code to run some checks on initialAge
+    }
+
+    func amIOld() {
+        // Do some computations in here and print out the correct statement to the console
+    }
+
+    func yearPasses() {
+        // Increment the age of the person in here
+    }
+}
+
+let t = Int(readLine()!)!
