81b58c6a6bbc4aeee9eb4acbe1e7703c7a035481
Inquis1t0r <pi0tr3k9292@gmail.com>
2022-10-25 14:06:55 +0200

    Update Day5:Loops.swift

--- 30DaysOfCode/Swift/Day5:Loops.swift
+++ 30DaysOfCode/Swift/Day5:Loops.swift
@@ -4,3 +4,7 @@ import Foundation
 
 guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
 else { fatalError("Bad input") }
+
+for i in 1 ... 10 {
+    print("\(n) x \(i) = \(n * i)");
+}
