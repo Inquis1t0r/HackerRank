a1fbf75b14af8dbc08bad9db7fb0566770507b30
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-07-02 20:03:11 +0200

    Update Day0DataTypes.js

--- 10DaysofJavascriptChallenges/Day0DataTypes.js
+++ 10DaysofJavascriptChallenges/Day0DataTypes.js
@@ -45,11 +45,11 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     const firstString = 'HackerRank ';
     
     // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
-    console.log(firstInteger + secondInteger);
+    console.log(firstInteger + Number(secondInteger));
     
     
     // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
-    console.log(firstDecimal + secondDecimal);
+    console.log(firstDecimal + Number(secondDecimal));
     
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
     console.log(firstString + secondString);
